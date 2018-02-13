ef1e23a09bfeb15860479afc350a0cc42b3d7d7e
apajitnov <alexei@pajitnov.info>
2018-02-13 17:04:36 -0500

    added directive

--- src/app/directives/promocontent/promo.directive.ts
+++ src/app/directives/promocontent/promo.directive.ts
@@ -1,10 +1,25 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appPromo]'
 })
 export class PromoDirective {
 
-  constructor() { }
+  constructor(private el: ElementRef) {
+  }
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.highlight('yellow');
+  }
+
+  @HostListener('mouseleave') onMouseLeave() {
+    this.highlight(null);
+  }
+
+  private highlight(color: string) {
+    this.el.nativeElement.style.backgroundColor = color;
+  }
 
 }
+
+
